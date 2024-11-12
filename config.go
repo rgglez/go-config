a2c93c70ca9b2efb92761811977a7014204f6ad5
Rodolfo Gonzalez <rodolfo.gonzalez+github@gmail.com>
2024-11-12 02:12:12 -0600

    Code

---
+++ config.go
@@ -0,0 +1,108 @@
+package config
+
+import (
+	"errors"
+	"path/filepath"
+
+	"log"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/rgglez/storage/storage"
+
+	yaml "gopkg.in/yaml.v3"
+)
+
+func LoadConfig(args map[string]string) (map[string]interface{}, error) {
+	var config map[string]interface{}
+
+	cnn := ""
+	domain := ""
+	stage := "prod"
+	file := "cfg.yaml"
+	path := "/tmp/"
+
+	for key, value := range args {
+		switch key {
+		case "cnn":
+			cnn = value
+		case "stage":
+			stage = value
+		case "file":
+			file = value
+		case "path":
+			path = value
+		case "referrer":
+			url, err := url.Parse(value)
+			if err != nil {
+				panic(err)
+			}
+			hostname := url.Hostname()
+			parts := strings.Split(hostname, ":")
+			domain = parts[0]
+			semiPath := "/tmp/" + domain
+			if _, err := os.Stat(semiPath); errors.Is(err, os.ErrNotExist) {
+				if err != nil {
+					err := os.Mkdir(semiPath, os.ModePerm)
+					if err != nil {
+						panic(err)
+					}
+				}
+			}
+		}
+	}
+
+	if strings.HasPrefix(cnn, "file://") {
+		thePath := strings.TrimPrefix(cnn, "file://")
+		path = filepath.Dir(thePath)
+	}
+
+	path = path + "/" + domain + "/" + file
+	re := regexp.MustCompile(`(\/)+`)
+	path = re.ReplaceAllStringFunc(path, func(m string) string {
+		return "/"
+	})
+
+	var data []byte
+
+	fileInfo, err := os.Stat(path)
+	if (err != nil || fileInfo.Size() == 0) && !strings.HasPrefix(cnn, "file://") {
+		// no existe el archivo local y es un recurso remoto, descargar
+		store := storage.NewStorage(cnn)
+
+		remotePath := domain + "/" + stage + "/" + file
+		re := regexp.MustCompile(`(\/)+`)
+		remotePath = re.ReplaceAllStringFunc(remotePath, func(m string) string {
+			return "/"
+		})
+		remotePath = strings.TrimLeft(remotePath, "/")
+
+		err := store.Read(remotePath, path)
+		if err != nil {
+			panic(remotePath)
+		}
+	}
+
+	fileInfo, err = os.Stat(path)
+	if err == nil {
+		mode := fileInfo.Mode()
+		if mode.IsRegular() {
+			data, err = os.ReadFile(path)
+			if err != nil {
+				panic(err)
+			}
+		}
+	} else {
+		os.Remove(path)
+		log.Fatalf("Cannot open config file or file not downloaded correctly: %s", path)
+	}
+
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	return config, nil
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/rgglez/go-config
+
+go 1.23.2
+
+require (
+	github.com/rgglez/storage v0.0.0-20241112072120-12e6b49dd799
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rgglez/go-storage/v5 v5.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	github.com/ztrue/tracerr v0.4.0 // indirect
+)
